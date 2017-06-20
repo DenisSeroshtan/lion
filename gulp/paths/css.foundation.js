df9a996890ac3a9247b0078a8f62332f880454ab
DenisSeroshtan <haron_777@mail.ru>
2017-06-20 17:15:33 +0500

    add timer into presention

--- gulp/paths/css.foundation.js
+++ gulp/paths/css.foundation.js
@@ -1,5 +1,6 @@
 'use strict';
 
 module.exports = [
-  './bower_components/normalize-css/normalize.css'
+  './bower_components/normalize-css/normalize.css',
+  './source/style/common/jquery.countdown.css'
 ];
