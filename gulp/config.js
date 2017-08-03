0d7b37bb26077de4d7d292dfc0936049909d8b17
DenisSeroshtan <haron_777@mail.ru>
2017-08-03 14:31:02 +0500

    change bower.json

--- gulp/config.js
+++ gulp/config.js
@@ -1,5 +1,5 @@
 module.exports = {
   root: './build',
   pathTransit: './source/foundation/svg4everybody.min.js',
-  autoprefixerConfig: ['last 3 version', '> 1%', 'Opera 12.1']
+  autoprefixerConfig: ['last 5 version', '> 1%', 'Opera 12.1', 'ie 8']
 };
